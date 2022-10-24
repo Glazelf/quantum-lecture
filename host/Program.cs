f3c93805e1bfe88cffd26b93663bd1fa9b92a822
Glazelf <gideon.steenbergen@gmail.com>
2022-10-24 14:42:04 +0200

    initial program.cs
    doesnt work yet

--- host/Program.cs
+++ host/Program.cs
@@ -1,5 +1,17 @@
 ﻿namespace quantum
 {
-
-
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a new quantum simulator
+            using (var sim = new QuantumSimulator())
+            {
+                // Run the quantum program
+                var result = HelloQ.Run(sim).Result;
+                // Print the result
+                System.Console.WriteLine($"Result: {result}");
+            }
+        }
+    }
 }
